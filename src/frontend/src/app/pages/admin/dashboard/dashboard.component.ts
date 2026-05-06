de981895e51c0423a0b964db768d860e9c6298cd
Joseph Joel Quispe Alvarez <JosephQA3131@gmail.com>
2026-05-06 22:34:58 +0200

    Reestructuración del proyecto y creación de componentes admin

---
+++ src/frontend/src/app/pages/admin/dashboard/dashboard.component.ts
@@ -0,0 +1,12 @@
+﻿import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { FooterComponent } from '../../../shared/layout/footer/footer.component';
+import { SidebarComponent } from '../../../shared/layout/sidebar/sidebar.component';
+
+@Component({
+  selector: 'app-dashboard',
+  imports: [RouterLink, FooterComponent, SidebarComponent],
+  templateUrl: './dashboard.component.html',
+  styleUrl: './dashboard.component.css'
+})
+export class DashboardComponent { }
---
+++ src/frontend/src/app/pages/admin/operarios/operarios.component.ts
@@ -0,0 +1,12 @@
+﻿import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { FooterComponent } from '../../../shared/layout/footer/footer.component';
+import { SidebarComponent } from '../../../shared/layout/sidebar/sidebar.component';
+
+@Component({
+  selector: 'app-operarios',
+  imports: [RouterLink, FooterComponent, SidebarComponent],
+  templateUrl: './operarios.component.html',
+  styleUrl: './operarios.component.css'
+})
+export class OperariosComponent { }
