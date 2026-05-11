9aaa7d47f7c7ead344bcd5e8ae7ed10f34198c31
Sergio <sergiodiazpereira.guadalupe@alumnado.fundacionloyola.net>
2026-05-11 14:23:40 +0200

    ACEPTAR TODO

--- src/vistas/inicioTicketsVacio.php
+++ src/vistas/inicioTicketsVacio.php
@@ -16,7 +16,7 @@
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
 	<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" rel="stylesheet">
-	<link rel="stylesheet" href="estiloUsuario.css">
+	<link rel="stylesheet" href="../css/estiloUsuario.css">
 </head>
 <body>
 	<header>
--- src/vistas/operarios_admin.php
+++ src/vistas/operarios_admin.php
@@ -19,7 +19,7 @@
     <!-- Google Fonts: Inter -->
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
     <!-- Estilos Propios -->
-    <link rel="stylesheet" href="estiloAdmin.css">
+    <link rel="stylesheet" href="../css/estiloAdmin.css">
 </head>
 
 <body id="pagina-dashboard">
